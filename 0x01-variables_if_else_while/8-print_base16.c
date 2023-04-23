e48b8251592a309a2ffd59a115dc6ef93f9ffb5f
khadija hiss <khadijahissou2@gmail.com>
2023-04-23 12:38:55 -0700

    C task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - initiation to c
+ * Return: the return is 0
+ */
+int main(void)
+{
+	int a;
+
+	while (a < 58)
+	{
+	putchar(a);
+	a++;
+	}
+	for (a = 48; a < 54; a++)
+	{
+	putchar(a + 49)
+	}
+	putchar('\n');
+	return (0);
+}
