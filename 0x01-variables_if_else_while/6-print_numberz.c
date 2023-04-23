5b60fab0fd2a6e883632a285f7de89909c07486c
khadija hiss <khadijahissou2@gmail.com>
2023-04-23 12:05:19 -0700

    C task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - initiation
+ * Return: returning 0
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+	putchar(x + 48);
+	x++;
+	
+	putchar('\n');
+	return (0);
+}
