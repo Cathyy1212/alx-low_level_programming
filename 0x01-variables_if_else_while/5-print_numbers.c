a91b2ddf83bb2501e58bf4a542817b9bb0ec0b98
khadija hiss <khadijahissou2@gmail.com>
2023-04-23 12:00:14 -0700

    C task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - initiation to C
+ * Return: the return is 0
+ */
+int main(void)
+{
+	int  x = 0;
+
+	while (x < 10)
+	{
+	printf("%d", x);
+	x++;
+	}
+	putchar('\n');
+	return (0);
+}
