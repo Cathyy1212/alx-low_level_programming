cd7f7048bab3f598754481cd9c31d349ee45aced
khadija hiss <khadijahissou2@gmail.com>
2023-04-23 11:36:40 -0700

    c task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - initiation to c
+ * Return: the returning is 0
+ */
+int main(void)
+{
+        char b = 'a';
+
+        while (b <= 'z')
+        {
+        if (b!= 'e' && b!= 'q')
+        {
+        putchar(b);
+        b++;
+        }
+        else
+        {
+        b++;
+        }
+        }
+        putchar('\n');
+        return (0);
+}
