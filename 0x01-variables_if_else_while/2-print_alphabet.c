9c4f48135158d9a9402f77491bb22496d19e187c
khadija hiss <khadijahissou2@gmail.com>
2023-04-23 11:03:27 -0700

    c task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - C language is so cool
+ * Return: Our return here is 0
+ */
+int main(void)
+{
+	char c='a';
+
+	while (c=<'z')
+	{
+	putchar(c);
+	c++;
+	}
+	putchar('\n');
+	return (0);
+}
