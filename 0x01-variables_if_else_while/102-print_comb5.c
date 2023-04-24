114fa55e7bb86e9d6e0191c9b778dcccad4aa911
khadija hiss <khadijahissou2@gmail.com>
2023-04-24 16:01:37 -0700

    102-print_comb3.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,19 +9,19 @@ int main(void)
 	int x, y;
 
 	x = 0;
-	while (x <=99)
+	while (x <= 99)
 	{
-	x = y;
-	while (y <=99)
+	y = x;
+	while (y <= 99)
 	{
 	if (y != x)
 	{
-	putchar((x/10)+48);
-	putchar((x%10)+48);
+	putchar((x / 10) + 48);
+	putchar((x % 10) + 48);
 	putchar(' ');
-	putchar((y/10)+48);
-	putchar((y%10)+48);
-	if (x !=98 ||y != 99)
+	putchar((y / 10) + 48);
+	putchar((y % 10) + 48);
+	if (x != 98 || y != 99)
 	{
 	putchar(',');
 	putchar(' ');
