83c4702bb004414196eb5f929feb2924fdc7b2a2
khadija hiss <khadijahissou2@gmail.com>
2023-04-24 15:51:48 -0700

    102-print_comb3.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - initiaion to C
+ * Return: returning 0
+ */
+int main(void)
+{
+	int x, y;
+
+	x = 0;
+	while (x <=99)
+	{
+	x = y;
+	while (y <=99)
+	{
+	if (y != a)
+	{
+	putchar((x/10)+48);
+	putchar((x%10)+48);
+	putchar(' ');
+	putchar((y/10)+48);
+	putchar((y%10)+48);
+	if (x !=98 ||y != 99)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	y++;
+	}
+	x++;
+	}
+	putchar('\n');
+	return (0);
+}
