7065ae00f838d0d5ea2fa5c2b8d592f1d3b7f005
khadija hiss <khadijahissou2@gmail.com>
2023-04-24 15:15:49 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**	
+ * main - initiation to c
+ * Return: returning 0
+ */
+int main(void)
+{
+	int x, y;
+
+	x = 0;
+	while (a < 10)
+	{
+	y = 1;
+	while (y < 10)
+	{
+	if (y <= a)
+	{
+	y++;
+	}
+	else
+	{
+	putchar (x + 48);
+	putchar(y + 48);
+	if (x + 48 == 56 && y + 48 == 57)
+	break;
+	else
+	{
+	putchar(',');
+	putchar(' ');
+	y++;
+	}
+	}
+	}
+	x++;
+	}
+	putchar('\n');
+	return (0);
+}
