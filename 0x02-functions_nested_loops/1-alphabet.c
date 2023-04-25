f7d675fa9fcdb970879ea3753c84998379308eec
khadija hiss <khadijahissou2@gmail.com>
2023-04-25 15:03:49 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - functions Nested loops
+ * Return: returning 0
+ */
+void print_alphabet(void)
+{
+	int f;
+	for (f = 97; f <= 122; f++)
+	{
+		_putchar(f);
+	}
+	_putchar('\n');
+}
