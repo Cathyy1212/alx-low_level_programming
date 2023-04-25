59a45a94b70aa90c420990f16c1ddf2bd589680b
khadija hiss <khadijahissou2@gmail.com>
2023-04-25 16:58:50 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * islower - shows 1 if the input is a
+ * _islower - shows 1 if the input is a
  * lowercase character. Another cases, shows
  * 0
+ *
  * @c: 1 for lowercase character. 0 for the rest.
  */
 int _islower(int c)
