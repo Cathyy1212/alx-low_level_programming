1d7d885337545847cafe34fe8f7e9ecf0b19e772
khadija hiss <khadijahissou2@gmail.com>
2023-04-26 09:06:29 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,32 +5,32 @@
  */
 void times_table(void)
 {
-    int i, j, res;
+	int a b, res;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (j = 0; j < 10; j++)
-        {
-            res = i * j;
-            if (j == 0)
-            {
-                _putchar('0');
-            }
-            else if (res < 10)
-            {
-                _putchar(',');
-                _putchar(' ');
-                _putchar(' ');
-                _putchar(res + '0');
-            }
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
-                _putchar((res / 10) + '0');
-                _putchar((res % 10) + '0');
-            }
-        }
-        _putchar('\n');
-    }
+	for (a = 0; a < 10; a++)
+	{
+	for (b = 0; b < 10; b++)
+	{
+		res = a * b;
+		if (b == 0)
+		{
+		_putchar('0');
+		}
+		else if (res < 10)
+		{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(res + '0');
+		}
+		else
+	    	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar((res / 10) + '0');
+		_putchar((res % 10) + '0');
+		}
+	}
+	_putchar('\n');
+	}
 }
