a474f858416b117e679b31dafd5d400a02db2789
khadija hiss <khadijahissou2@gmail.com>
2023-04-26 09:43:58 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-#include "main.h"/**
- * main - check the code
- * @c: describe the
- * Return: returning 0
- */
+#include "main.h"
 
-int main(void) {
-	int result = add(a, b);
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int result = add(5, 6);
 	printf("Result: %d\n", result);
 	return 0;
 }
