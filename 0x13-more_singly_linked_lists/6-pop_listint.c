8714d62da6d2acc940ee5ef13b95181d1f6267a0
khadija hiss <khadijahissou2@gmail.com>
2023-06-06 16:04:00 -0700

    0x13

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -7,16 +7,16 @@
  */
 int pop_listint(listint_t **head)
 {
-        listint_t *a;
-        int b;
+	listint_t *a;
+	int b;
 
-        if (!head || !*head)
-                return (0);
+	if (!head || !*head)
+		return (0);
 
-        b = (*head)->n;
-        a = (*head)->next;
-        free(*head);
-        *head = a;
+	b = (*head)->n;
+	a = (*head)->next;
+	free(*head);
+	*head = a;
 
-        return (b);
+	return (b);
 }
