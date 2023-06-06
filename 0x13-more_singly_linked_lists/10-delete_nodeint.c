e63282b84efe9840b9360e4e23028b1455fb3e1b
khadija hiss <khadijahissou2@gmail.com>
2023-06-06 16:22:35 -0700

    0x13

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@ int delete_nodeint_at_index(listint_t **list, unsigned int index)
 	listint_t *current = NULL;
 	unsigned int i = 0;
 
-	 if (*list == NULL)
+	if (*list == NULL)
 		return (-1);
 
 	if (index == 0)
