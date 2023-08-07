b0d40a40c95f64cb2fb9c660f13d63de5cd9df40
Cathyy1212 <khadijahissou2@gmail.com>
2023-08-07 16:07:55 -0700

    0x15CC

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,15 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-#ifndef ELF_HEADER_FUNCTIONS_H
-#define ELF_HEADER_FUNCTIONS_H
-
-#include <elf.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -19,6 +9,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
-void check_elf_header(unsigned char *e_ident);
-void print_magic_numbers(unsigned char *e_ident);
 #endif
